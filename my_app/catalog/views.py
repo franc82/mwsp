990da7a7bb9a931ae81cb6ada9606612fbeffce3
Franclin Foping <franclin.foping@nitrosell.net>
2020-12-29 00:36:47 +0000

    [fix] ip address reporting

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -110,9 +110,9 @@ def contact():
                       )
 
         success = True
-        for k,v in request.headers.items():
+        for k,v in request.environ.items():
             print('key {}, val {}'.format(k,v))
-            
+
         objMessage = ContactMessage(fullname=message.get('name'),
                                     email=message.get('email'),
                                     message=message.get('message'),
